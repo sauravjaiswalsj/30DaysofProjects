5677248d092635ff56908690edbe2cca6f09d07a
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-04-19 20:02:13 +0530

    Adding final codes

--- js30/webdrum/index.js
+++ js30/webdrum/index.js
@@ -1,49 +1,88 @@
-
+const item_1 = document.querySelector('#b1');
+const item_2 = document.querySelector('#b2');
+const item_3 = document.querySelector('#b3');
+const item_4 = document.querySelector('#b4');
+const item_5 = document.querySelector('#b5');
+const item_6 = document.querySelector('#b6');
+const item_7 = document.querySelector('#b7');
+const item_8 = document.querySelector('#b8');
+const item_9 = document.querySelector('#b9');
 const play = (e) =>{
     e.currentTime=0; // rewind to start
     e.play();
+    console.log(e);
+}
+const Transition=(e)=>{
+    const transition = new Queue();
+    transition.enqueue(e.classList.add('playing'));
+    setTimeout(()=>{
+        Transition.dequeue(e.classList.remove('playing'));
+    },10);
 }
 window.addEventListener('keydown', (e)=>{
    const key= e.which || e.keyCode;
    if(key===65){ // A
        const audio = new Audio('sounds/clap.wav');
        play(audio);
+        Transition(item_1);
    }
    else if(key===83){  // S
        const audio = new Audio('sounds/boom.wav');
        play(audio);
+       Transition(item_2);
    }
    else if(key===68){  //D
        const audio=new Audio('sounds/hihat.wav');
        play(audio);
+       Transition(item_3);
    }
    else if(key=== 70){ //F
        const audio = new Audio('sounds/kick.wav');
        play(audio);
+      Transition(item_4);
    }
    else if(key=== 71){ //G
     const audio = new Audio('sounds/openhat.wav');
     play(audio);
+    Transition(item_5);
 } else if(key=== 72){ //H
     const audio = new Audio('sounds/snare.wav');
     play(audio);
+    Transition(item_6);
 }else if(key=== 74){ // J
     const audio = new Audio('sounds/tink.wav');
     play(audio);
+    Transition(item_7);
 }
 else if(key=== 75){ // K
     const audio = new Audio('sounds/ride.wav');
     play(audio);
+    Transition(item_8);
 }
 else if(key=== 76){ // L
     const audio = new Audio('sounds/tom.wav');
     play(audio);
+    Transition(item_9);
 }
    else {
        return ;
    }
 });
 
+class Queue{
+    constructor(){
+        this.item=[];
+    }
+    enqueue(data){
+        this.item.unshift(data);
+    }
+    dequeue(){
+        if(this.isEmpty()){
+            return "Underflow";
+        }
+        return this.item.pop();
+    }
+}
 
 
 /************************particle js************************ */
