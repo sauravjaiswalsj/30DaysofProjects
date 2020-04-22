3d9d5558e89094e72b5a0a9badd581b7d584aeb7
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-04-23 01:33:29 +0530

    adding:codes

--- js30/clock/script.js
+++ js30/clock/script.js
@@ -1,2 +1,13 @@
 //js
 const js=document.getElementById();
+.body{
+    background-color: black;
+}
+.container{
+    align-items: center;
+}
+.clock{
+    background-color:lightblue;
+    border-radius: 50%;
+
+}
