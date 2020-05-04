7699f39da9ed7650b808ea029dc5ee6c471b5026
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-05-05 00:06:44 +0530

    board-code

--- cpp-nanodegree/class/board.cpp
+++ cpp-nanodegree/class/board.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include<vector>
+#include<fstream>
+using std::ifstream;
 #define fo1 for(int i=0;i<board.size();i++)
 #define fo2 for(int j=0;j<board.size();j++)
 using std::vector;
 using std::cout;
-
+using std::string;
 void PrintBoard(vector<vector<int>> board){
 	fo1{
 		fo2{
@@ -13,7 +15,14 @@ void PrintBoard(vector<vector<int>> board){
 		cout<<"\n";
 	}
 }
-
+void ReadBoardFile(string file){
+	ifstream myFile;
+	myFile.open(file);
+	std::string line;
+	while(getline(myFile, line)){
+		cout<<line<<"\n";
+	}
+}
 int main(void){
 	vector<vector<int>> board{{0, 1, 0, 0, 0, 0},
 {0, 1, 0, 0, 0, 0},
@@ -21,6 +30,8 @@ int main(void){
 {0, 1, 0, 0, 0, 0},
 {0, 0, 0, 0, 1, 0}};
 	PrintBoard(board);
+	string file="board.txt";
+	ReadBoardFile(file);
 	return 0;
 }
 
