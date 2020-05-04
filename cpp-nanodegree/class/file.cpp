97ff11bb12d8deed37eee05680e436c6d8943550
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-05-04 23:37:32 +0530

    cpp notes

--- cpp-nanodegree/class/file.cpp
+++ cpp-nanodegree/class/file.cpp
@@ -8,4 +8,15 @@ using std::ifstream;
 
 int main(){
 	ifstream fileName;
+	fileName.open("2d.cpp");
+	if(fileName){
+		cout<<"file exist"<<"\n";
+	}
+	std::string line;
+	while(getline(fileName, line)){
+		cout<<line<<" ";
+	}
+	
 
+
+}
