6e0a88415eda4f52e262866d22d977c5231a9e3b
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-08-20 23:26:27 +0530

    adding Map.js

---
+++ js30/frontend-master/Map.js
@@ -0,0 +1,16 @@
+// difference between each and map is that _.each does not return anything from their callback function.
+// Map returns an array.
+
+// _.map([1,2,3],function(val){
+//     return val*2;
+// });
+
+const _={};
+ _.map = (list,callback)=>{
+    var newArray=[];
+    for(let i=0;i<list.length;i++){
+        
+    }
+    return newArray;
+ };
+
