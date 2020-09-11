5f7e064c5702560de6cf6423ed201b45f171d70b
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-09-11 11:42:06 +0530

    Completed

--- Java-dev/src/tictactoe/TicTacToe.java
+++ Java-dev/src/tictactoe/TicTacToe.java
@@ -47,6 +47,47 @@ class BoardSimulation{
         }
         System.out.println("\t\t\t\t  _______________");
     }
+    /**
+     *  Check for winner
+     *
+     */
+    class State{
+        boolean check=false;
+        char value='_';
+    }
+    protected ArrayList<State>  winnerChecker(){
+        State s= new State();
+        ArrayList<State> li = new ArrayList<>();
+        for(int i=0;i<board.length;i++){
+            for(int j=1;j<board[i].length-1;j++) {
+                if ((board[i][j] == board[i][j - 1]) && (board[i][j] == board[i][j + 1])) {
+                    s.check=true;
+                    s.value=board[i][j];
+                    li.add(s);
+                    return li;
+                } else if ((board[j - 1][i] == board[j][i]) && ((j+1<board.length)&&(board[j + 1][i] == board[j][i]))){
+                    s.check=true;
+                    s.value=board[i][j];
+                    li.add(s);
+                    return li;
+                }
+            }
+        }
+        if((board[0][0]==board[1][1]) && (board[1][1]==board[2][2])){
+            s.check=true;
+            s.value=board[1][1];
+            li.add(s);
+        }else if((board[0][2]==board[1][1]) &&(board[2][0]==board[1][1])){
+            s.check=true;
+            s.value=board[1][1];
+            li.add(s);
+        }else{
+            s.check=false;
+            s.value='-';
+            li.add(s);
+        }
+        return li;
+    }
 }
 
 /**
@@ -72,9 +113,16 @@ public class TicTacToe {
         System.out.println();
         String input = br.readLine();
         simulation.setBoard(to2dArray(input));
-
+        final ArrayList<BoardSimulation.State> states = simulation.winnerChecker();
         // Display board
         simulation.displayBoard();
+
+        // Print winner
+        if(states.get(0).check){
+            System.out.println(states.get(0).value + " has won the game");
+        }else{
+            System.out.print("Draw");
+        }
     }
 
     /**
