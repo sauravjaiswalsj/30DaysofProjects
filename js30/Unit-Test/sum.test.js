0b91bddc5f4ede09eb2e927629b2358061733934
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-09-16 23:24:06 +0530

    adding Test

--- js30/Unit-Test/sum.test.js
+++ js30/Unit-Test/sum.test.js
@@ -1,6 +1,11 @@
+// This uses jest library for the file
 const { exportAllDeclaration } = require('@babel/types');
 const sum = require('./sum');
 
+
+// this is to test file 
+// @param (sum(a,b)) equals sum
+// check the output agains the actual output
 test('adds 1 + 2 to equal 3 ', ()=>{
  expect(sum(1,2)).toBe(3);
 });
