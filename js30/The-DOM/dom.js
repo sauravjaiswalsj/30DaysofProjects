bc6bf63eb368cd99d029b826cfbfa540959dc771
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-10-17 19:58:56 +0530

    adding:DOM Features

--- js30/The-DOM/dom.js
+++ js30/The-DOM/dom.js
@@ -5,3 +5,30 @@ const odd = document.querySelectorAll('li:nth-child(odd');
 odd.forEach(item=>{
     item.style.backgroundColor ="#f4f4f4";
 });
+console.log(document.querySelector('#items').parentNode);
+console.log(document.querySelector('#items').children);
+
+console.log(document.querySelector('#items').firstElementChild);
+
+// let newDiv = document.createElement('div');
+// newDiv.className='hello';
+// newDiv.id='hello1';
+// newDiv.setAttribute('title','Hello DIv');
+
+// var newDivText =  document.createTextNode('Hello World');
+
+// newDiv.appendChild(newDivText); 
+// console.log(newDiv);
+
+// let container = document.querySelector('.container');
+
+// var h2 = document.querySelector('h2 .title');
+
+// container.insertBefore(newDiv,h2);
+const form = document.querySelector('form');
+const button= document.querySelector('button');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    console.log(e.type);
+});
