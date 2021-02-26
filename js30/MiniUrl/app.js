49aa08645e08bdc22842a571f0b2212416ddc731
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2021-02-26 11:32:33 +0530

    adding new path in app

--- js30/MiniUrl/app.js
+++ js30/MiniUrl/app.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
 const express = require('express');
 const app = express();
+const pug = require('pug');
 const route = require('./Routes/route');
 const port =3000;
+
+//set the view engine
+app.set('view engine','pug');
+// set the path
+app.set('views','./views');
 
 //route
 app.use(route.router);
