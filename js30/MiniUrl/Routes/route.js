ae576920ed73236096719a29047a8515942bff61
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2021-02-26 11:31:43 +0530

    Adding new routes

--- js30/MiniUrl/Routes/route.js
+++ js30/MiniUrl/Routes/route.js
@@ -7,15 +7,15 @@ const router = express.Router();
 // Path is path on the server
 //Handler is the function to be excuted when the route is matched
 router.get('/', (req,res)=>{
-    console.log('Hllo')
    // res.send('Url Shortner');
-    res.render('index',{titel:"Url Shortner",message:"Short any URL"});
+    res.render('index',{ title: 'Url Shortner', message: 'Short any URL'});
 });
 router.get('/about',(req,res)=>{
-    res.send('About the page');
+    //res.send('About the page');
+    res.render('about',{title:'About', message:'About the Page'});
 });
 router.get('/contact',(req,res)=>{
-    console.log('Contact me over mail');
-    res.send('Contact me over mail');
+    res.render('contact',{title:'Contact',message:'Contact the admin'});
+    //res.send('Contact me over mail');
 });
 module.exports = {router};
